e08504c350f7cb8a252e583642bed0a0a7f13ba2
Chin2411 <berdchin@gmail.com>
2025-06-01 20:06:32 +0300

    Add smart PDF extraction service

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,3 +1,4 @@
 from .file_metadata import extract_metadata
 from .file_preview import extract_preview, extract_preview_text
+from .smart_pdf import smart_pdf_extract
 from .document_workflow import DocumentWorkflow, FileInfo
