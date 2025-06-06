27e8a5d26b2621478e06e76f9445150ff83b15cb
Chin2411 <berdchin@gmail.com>
2025-06-06 16:33:44 +0300

    Add reference dictionary and integrate into checks

---
+++ src/data_analysis/reference_data.py
@@ -0,0 +1,12 @@
+reference_data = {
+    "Акт": "Act",
+    "Инструкция по эксплуатации": "User Manual",
+    "Техническое задание": "Terms of reference / Technical specification",
+    "Сертификат соответствия": "Certificate of Conformity",
+    "Руководство по эксплуатации": "User Guide",
+    "Паспорт": "Passport",
+    "График": "Schedule",
+    "Договор": "Contract",
+    "Смета": "Cost estimate",
+    "Коммерческое предложение": "Commercial offer",
+}
